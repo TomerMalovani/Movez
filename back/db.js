733e1dc88cc9224f1746b42775ae5d7750efe1a5
Malo <51359873+TomerMalovani@users.noreply.github.com>
2024-05-06 22:03:09 +0300

    Update db.js
    remove secrets

--- back/db.js
+++ back/db.js
@@ -2,11 +2,11 @@ const Pool = require("pg").Pool;
 const bcrypt = require("bcrypt");
 
 const pool = new Pool({
-  user: "movez",
-  password: "movez",
-  host: "4.156.32.216",
-  port: 5432,
-  database: "balance"
+  user: ,
+  password: ,
+  host: ,
+  port: ,
+  database: 
 });
 
 
