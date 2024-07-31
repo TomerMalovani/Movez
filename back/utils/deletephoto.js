40d73e8f962da972f6e018725dbe471b4a813294
tomermalovani <tomermalovani@gmail.com>
2024-07-31 19:34:33 +0300

    removed secret

--- back/utils/deletephoto.js
+++ back/utils/deletephoto.js
@@ -1,5 +1,5 @@
 const { BlobServiceClient } = require('@azure/storage-blob');
-const AZURE_STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName=movezblobstorage;AccountKey=gdynUtIh+okgMWIeg3pLYYMNk5rXhIG/h0IiQ1/BqYNy4TqgJowOKQLPR0So9hYBww6+aH30mndD+AStU7DwQg==;EndpointSuffix=core.windows.net'; //change later
+const AZURE_STORAGE_CONNECTION_STRING = ''
 const containerName = 'photos';
 
 const deletePhoto = async (photoUrl) => {
