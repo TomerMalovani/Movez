dafa964ea41b97db8dee93994b67a26847b43212
tomermalovani <tomermalovani@gmail.com>
2024-09-06 20:43:16 +0300

    fixed back buttons and scrolls

--- front/movez/screens/MovesProvided.js
+++ front/movez/screens/MovesProvided.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import MoveProvidingList from '../components/MoveProvidingList';
+import { Appbar } from 'react-native-paper';
 
 const MovesProvided = ({ navigation, route }) => {
     const selectedVehicle = route.params?.selectedVehicle;
     console.log("Selected vehicle in MovesProvided:", selectedVehicle);
 
     return (
+		<>
+		<Appbar.BackAction onPress={() => { navigation.navigate('Home') }} />
+
+	
         <MoveProvidingList 
             navigation={navigation} 
             filterStatus={['Pending']} 
         />
+		</>
     );
 };
 
--- front/movez/screens/ProvidingsHistory.js
+++ front/movez/screens/ProvidingsHistory.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import MoveProvidingList from '../components/MoveProvidingList';
+import { Appbar } from 'react-native-paper';
 
 const ProvidingsHistory = ({ navigation }) => {
     return (
-        <MoveProvidingList navigation={navigation} filterStatus={['Canceled', 'Done']} />
+       <> 
+			<Appbar.BackAction onPress={() => { navigation.navigate('Home') }} />
+
+	   <MoveProvidingList navigation={navigation} filterStatus={['Canceled', 'Done']} />
+	   </>
     );
 };
 
